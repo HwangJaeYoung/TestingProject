2d3a37eea590166903ad7be14aa8799445c94240
HwangJaeYoung <forest62590@naver.com>
2016-06-09 21:51:52 +0900

    Adding the resource add function

--- oneM2MTester/app.js
+++ oneM2MTester/app.js
@@ -18,9 +18,9 @@ app.get('/', function (reqeust, response) {
 
     var client = dbClient.getDBClient(); // Getting Database information.
 
-    client.query('SELECT * FROM onem2m', function (error, results, fields) {
+    client.query('SELECT * FROM onem2m ORDER BY time ASC', function (error, results, fields) {
         if (error) { // error
-            console.log("Database resource retrieve error : " + error);
+            console.log("MySQL : Database resource retrieve error : " + error);
             response.writeHead(500, {'Content-Type': 'text.html'});
             response.end();
         } else { // success
@@ -41,11 +41,11 @@ app.get('/', function (reqeust, response) {
             }
             jsonObject.resourceInfo = jsonArray;
 
-            console.log('Success retrieving the resource');
+            console.log('MySQL : Success retrieving the resource');
 
             fs.readFile('TestingView.ejs', 'utf-8', function (error, data) {
                 response.writeHead(200, {'Content-Type': 'text.html'});
-                response.end(ejs.render(data, { resoruceConfig : JSON.stringify(jsonObject) }));
+                response.end(ejs.render(data, { resourceConfig : JSON.stringify(jsonObject) }));
             });
         }
     });
@@ -59,13 +59,27 @@ app.post('/createResource', function (request, response) {
 
     var client = dbClient.getDBClient(); // Getting Database information.
 
-    client.query('INSERT INTO onem2m (resourceName, resourceTitle) VALUES (?, ?)', [resourceName, resourceTitle], function (error, results, fields) {
+    var cur_d = new Date();
+
+    var msec = '';
+
+    if((parseInt(cur_d.getMilliseconds(), 10) < 10)) {
+        msec = ('00' + cur_d.getMilliseconds());
+    } else if((parseInt(cur_d.getMilliseconds(), 10) < 100)) {
+        msec = ('0' + cur_d.getMilliseconds());
+    } else {
+        msec = cur_d.getMilliseconds();
+    }
+
+    var time = "" + cur_d.getFullYear() + (cur_d.getMonth() + 1) + cur_d.getDate() + cur_d.getHours() + cur_d.getMinutes( ) + cur_d.getSeconds() + msec;
+
+    client.query('INSERT INTO onem2m (resourceName, resourceTitle, time) VALUES (?, ?, ?)', [resourceName, resourceTitle, time], function (error, results, fields) {
         if (error) { // error
-            console.log("Database resource creation error : " + error);
+            console.log("MySQL : Database resource creation error : " + error);
             response.writeHead(500, {'Content-Type': 'text.html'});
             response.end();
         } else { // success
-            console.log('Success creating the resource : ' + resourceName);
+            console.log('MySQL : Success creating the resource : ' + resourceName);
             response.writeHead(201, {'Content-Type': 'text.html'});
             response.end();
         }
